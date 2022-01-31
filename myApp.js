8e02c04a65af0d62cff8cb762f2c3e7d3e908fa6
Paul Cedo <me.paulfolio@gmail.com>
2022-01-31 22:15:17 +0800

    used helmet

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ const express = require('express');
 const helmet = require("helmet");
 const app = express();
 
-
+app.use(helmet());
 
 
 
@@ -53,10 +53,10 @@ const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/", function(request, response) {
+    response.sendFile(__dirname + '/views/index.html');
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+    console.log(`Your app is listening on port ${port}`);
 });
